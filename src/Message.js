ca3768645dc175f388253b676ece41aae7ef3388
asad149 <asad.arshad.94849@gmail.com>
2020-06-22 01:32:16 +0500

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Message(props) {
-  return <h3>The value of counter variable is: {props.counter} </h3>;
+  return <h3>The value of the counter variable is: {props.counter} </h3>;
 }
 
 export default Message;
